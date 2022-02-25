Fix mask calculation and segm plotting (for real this time)

dtron_cb/model.py
@@ -4,17 +4,113 @@ import torch
 
 from detectron2.config import configurable
 from detectron2.structures import ImageList, Instances, ROIMasks
-from detectron2.modeling.postprocessing import detector_postprocess
 from detectron2.modeling.meta_arch.rcnn import GeneralizedRCNN
 from detectron2.modeling.meta_arch.build import META_ARCH_REGISTRY
+from detectron2.utils.memory import retry_if_cuda_oom
+
+
+def get_paste_func(output_height):
+    from detectron2.layers.mask_ops import paste_masks_in_image, _paste_masks_tensor_shape
+
+    if torch.jit.is_tracing():
+        if isinstance(output_height, torch.Tensor):
+            paste_func = _paste_masks_tensor_shape
+        else:
+            paste_func = paste_masks_in_image
+    else:
+        paste_func = retry_if_cuda_oom(paste_masks_in_image)
+
+    return paste_func
+
+
+# perhaps should rename to "resize_instance"
+def detector_postprocess(
+    results: Instances, output_height: int, output_width: int, mask_threshold: float = 0.5
+):
+    """
+
+    copy of "from detectron2.modeling.postprocessing import detector_postprocess"
+
+    Resize the output instances.
+    The input images are often resized when entering an object detector.
+    As a result, we often need the outputs of the detector in a different
+    resolution from its inputs.
+
+    This function will resize the raw outputs of an R-CNN detector
+    to produce outputs according to the desired output resolution.
+
+    Args:
+        results (Instances): the raw outputs from the detector.
+            `results.image_size` contains the input image resolution the detector sees.
+            This object might be modified in-place.
+        output_height, output_width: the desired output resolution.
+
+    Returns:
+        Instances: the resized output from the model, based on the output resolution
+    """
+    if isinstance(output_width, torch.Tensor):
+        # This shape might (but not necessarily) be tensors during tracing.
+        # Converts integer tensors to float temporaries to ensure true
+        # division is performed when computing scale_x and scale_y.
+        output_width_tmp = output_width.float()
+        output_height_tmp = output_height.float()
+        new_size = torch.stack([output_height, output_width])
+    else:
+        new_size = (output_height, output_width)
+        output_width_tmp = output_width
+        output_height_tmp = output_height
+
+    scale_x, scale_y = (
+        output_width_tmp / results.image_size[1],
+        output_height_tmp / results.image_size[0],
+    )
+    results = Instances(new_size, **results.get_fields())
+
+    if results.has("pred_boxes"):
+        output_boxes = results.pred_boxes
+    elif results.has("proposal_boxes"):
+        output_boxes = results.proposal_boxes
+    else:
+        output_boxes = None
+    assert output_boxes is not None, "Predictions must contain boxes!"
+
+    output_boxes.scale(scale_x, scale_y)
+    output_boxes.clip(results.image_size)
+
+    results = results[output_boxes.nonempty()]
+
+    if results.has("pred_masks"):
+        if isinstance(results.pred_masks, ROIMasks):
+            roi_masks = results.pred_masks
+        else:
+            # pred_masks is a tensor of shape (N, 1, M, M)
+            roi_masks = ROIMasks(results.pred_masks[:, 0, :, :])
+
+        paste_func = get_paste_func(output_height)
+
+        assert 0.0 <= mask_threshold <= 1.0, mask_threshold
+
+        results.pred_masks = roi_masks.to_bitmasks(
+            results.pred_boxes, output_height, output_width, mask_threshold
+        ).tensor
+
+        pred_prob_masks = paste_func(roi_masks.tensor, results.pred_boxes.tensor, (output_height, output_width), threshold=-1.0)
+        results.pred_prob_masks = pred_prob_masks.to(float)/255.  # convert 0-255 back to 0-1 probabilities
+
+    if results.has("pred_keypoints"):
+        results.pred_keypoints[:, :, 0] *= scale_x
+        results.pred_keypoints[:, :, 1] *= scale_y
+
+    return results
 
 
 @META_ARCH_REGISTRY.register
 class CB_GeneralizedRCNN(GeneralizedRCNN):
 
     @configurable
-    def __init__(self, px_thresh, **kwargs):
+    def __init__(self, px_thresh, ov_thresh, **kwargs):
         self.px_thresh = px_thresh
+        self.ov_thresh = ov_thresh
         super().__init__(**kwargs)
 
     @classmethod
@@ -23,6 +119,7 @@ class CB_GeneralizedRCNN(GeneralizedRCNN):
         print(d)
         return {
             'px_thresh': cfg.INFERENCE.PIXEL_THRESH,
+            'ov_thresh': cfg.INFERENCE.OVERALL_THRESH,
             **d
         }
 

dtron_cb/predictor.py
@@ -204,92 +204,104 @@ class COCOPredictor:
 
         particles = Particles()
 
-        for d in tqdm(ds):
-            d = dict(**d)
-            fn = d['file_name']
-            del d['file_name']
+        with torch.no_grad():
+            for d in tqdm(ds):
+                d = dict(**d)
+                fn = d['file_name']
+                del d['file_name']
 
-            imdata = Image(file_name=os.path.relpath(fn, self.datasets_root), **d)
-            annotated_dataset.images.append(imdata)
+                imdata = Image(file_name=os.path.relpath(fn, self.datasets_root), **d)
+                annotated_dataset.images.append(imdata)
 
-            oim = cv2.imread(fn)
-            oimc = oim.copy()
-            n = 0
+                oim = cv2.imread(fn)
+                oimc = oim.copy()
+                n = 0
 
-            if self.crop:
-                x1, y1, x2, y2 = self.crop
-                oim = oim[y1:y2, x1:x2]
+                if self.crop:
+                    x1, y1, x2, y2 = self.crop
+                    oim = oim[y1:y2, x1:x2]
 
-            im_ident = fn.replace('/', '-').replace('\\', '-')[:-4]
-            # im = cv2.resize(im, (512, 512))
-            # im = torch.as_tensor(oim.astype('float32').transpose(2, 0, 1))
-            im = torch.as_tensor(oim.astype('float32')); im = im.permute((2, 0, 1))
-            inputs = [dict(image=im)]
-            instances = self.model.inference(inputs, do_postprocess=True)[0]['instances']
-
-            try:
-                scores = instances.scores
-                bboxes = instances.pred_boxes
-                cats = instances.pred_classes
-                masks = instances.pred_masks
-            except AttributeError:
-                print(list(instances.get_fields().keys()))
-                raise
-
-            composite = cv2.cvtColor(oim, cv2.COLOR_BGR2RGB)
-            for i, (mask_t, score, bbox, cat) in enumerate(zip(masks, scores, bboxes, cats)):
-
-                x1, y1, x2, y2 = bbox
-                bbox = x, y, w, h = int(x1), int(y1), int(x2-x1), int(y2-y1)
-                area = w*h
-                score = float(score.cpu())
-                if score < self.overall_thresh:
-                    continue
-
-                mask = (mask_t.cpu().numpy()*255).astype(np.uint8)
-                cnt = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
-                # if multiple contours are found; just take the largest
-                if len(cnt) > 1:
-                    cnt = sorted(cnt, key=lambda c: cv2.contourArea(c))[-1]
-                else:
-                    cnt = cnt[0]
+                im_ident = fn.replace('/', '-').replace('\\', '-')[:-4]
+                # im = cv2.resize(im, (512, 512))
+                # im = torch.as_tensor(oim.astype('float32').transpose(2, 0, 1))
+                im = torch.as_tensor(oim.astype('float32')); im = im.permute((2, 0, 1))
+                inputs = [dict(image=im)]
+                instances = self.model.inference(inputs, do_postprocess=True)[0]['instances']
 
                 try:
-                    particles.add(fn, oimc, cnt, self.px2um, float(score))
-                except ParticleConstructionError as e:
-                    print(f'Not adding particle: {e}')
-                    continue
+                    scores = instances.scores
+                    bboxes = instances.pred_boxes
+                    cats = instances.pred_classes
+                    masks = instances.pred_masks
+                    mask_probs = instances.pred_prob_masks
+                except AttributeError:
+                    print(list(instances.get_fields().keys()))
+                    raise
 
-                c = [ci*255 for ci in plt.cm.viridis(score)[:3]]
-                cv2.drawContours(composite, [cnt], 0, c, 2)
-                n += 1
+                composite = cv2.cvtColor(oim, cv2.COLOR_BGR2RGB)
+                for i, (maskb_t, maskf_t, score, bbox, cat) in enumerate(zip(masks, mask_probs, scores, bboxes, cats)):
 
-                # convert [[[x, y]], ... ] format to [x, y, x, y, ...]
-                cnt = np.array(cnt)
-                cnt = cnt.squeeze()
-                seg = np.zeros(cnt.size)
-                seg[::2] = cnt[:, 0]
-                seg[1::2] = cnt[:, 1]
-                seg = [[int(s) for s in seg]]
+                    x1, y1, x2, y2 = bbox.detach().cpu().numpy().astype(int)  # converts to numpy int32
+                    # conert to plain int or json serialiser will complain
+                    bbox = x, y, w, h = int(x1), int(y1), int(x2 - x1), int(y2 - y1)
+                    area = w*h
+                    score = float(score.cpu())
+                    if score < self.overall_thresh:
+                        continue
 
-                anndata = Annotation(
-                    idx=len(annotated_dataset.annotations),
-                    image_idx=int(imdata.idx),
-                    category_idx=cid2did[int(cat)],
-                    bbox=bbox,
-                    segmentation=seg,
-                    area=area
-                )
-                annotated_dataset.annotations.append(anndata)
+                    masku: np.ndarray = maskb_t.cpu().numpy()*255
+                    maskf: np.ndarray = maskf_t.cpu().numpy()
 
-                submask = (plt.cm.viridis(mask)*255).astype(np.uint8)
-                alpha_s = np.where(mask < 5, 0.0, 0.3)
-                alpha_c = 1.0 - alpha_s
-                for c in range(3):
-                    composite[:, :, c] = submask[:, :, c] * alpha_s + composite[:, :, c] * alpha_c
-            composite = cv2.cvtColor(composite, cv2.COLOR_BGR2RGB)
-            cv2.imwrite(f'{self.images_dir}/{im_ident}_n={n}.png', composite)
+                    # mask is uint mat in range 0-255, maskf is float mat in range 0-1
+                    cnt = cv2.findContours(masku, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
+                    # if multiple contours are found; just take the largest
+                    if len(cnt) > 1:
+                        cnt = sorted(cnt, key=lambda c: cv2.contourArea(c))[-1]
+                    else:
+                        cnt = cnt[0]
 
+                    try:
+                        particles.add(fn, oimc, cnt, self.px2um, float(score))
+                        n += 1
+                    except ParticleConstructionError as e:
+                        print(f'Not adding particle: {e}')
+                        continue
+
+                    # c = [ci*255 for ci in plt.cm.viridis(score)[:3]]
+                    # cv2.drawContours(composite, [cnt], 0, c, 2)
+
+                    # Draw bounding box
+                    bbox_contour = np.array([[x1, y1], [x2, y1], [x2, y2], [x1, y2], [x1, y1]])
+                    c = [ci*255 for ci in plt.cm.viridis(score)[:3]]
+                    cv2.drawContours(composite, [bbox_contour], 0, c, 2)
+
+                    # convert [[[x, y]], ... ] format to [x, y, x, y, ...]
+                    cnt = np.array(cnt)
+                    cnt = cnt.squeeze()
+                    seg = np.zeros(cnt.size)
+                    seg[::2] = cnt[:, 0]
+                    seg[1::2] = cnt[:, 1]
+                    seg = [[int(s) for s in seg]]
+
+                    anndata = Annotation(
+                        idx=len(annotated_dataset.annotations),
+                        image_idx=int(imdata.idx),
+                        category_idx=cid2did[int(cat)],
+                        bbox=bbox,
+                        segmentation=seg,
+                        area=area
+                    )
+                    annotated_dataset.annotations.append(anndata)
+
+                    # Draw pixel probabilities
+                    submask = (plt.cm.viridis(maskf)*255).astype(np.uint8)
+                    alpha_s = np.where(masku < 5, 0.0, 0.3)
+                    alpha_c = 1.0 - alpha_s
+                    for c in range(3):
+                        composite[:, :, c] = submask[:, :, c] * alpha_s + composite[:, :, c] * alpha_c
+
+                composite = cv2.cvtColor(composite, cv2.COLOR_BGR2RGB)
+                cv2.imwrite(f'{self.images_dir}/{im_ident}_n={n}.png', composite)
 
         annotated_dataset.write_out(f'{self.output_dir}/annot_{today()}_{ds_name}.json')
 
@@ -350,9 +362,13 @@ class COCOPredictor:
         for i, particles in enumerate(particless):
             y[i] = len(particles)
 
+        xtl = ['...'+c[0][-10:] for c in chunks]
+        xt = x
+
         max_ticks = 10
-        xtl = ['...'+c[0][-10:] for c in chunks][::len(x)//max_ticks]
-        xt = x[::len(x)//max_ticks]
+        if len(x) > max_ticks:
+            xt = xt[::len(x)//max_ticks]
+            xtl = xtl[::len(x)//max_ticks]
 
         plt.figure()
         plt.plot(x, y)

dtron_cb/utils/instances_to_mask.py
@@ -24,14 +24,13 @@ def _gt_instances_to_mask(instances: Instances):
     return combined
 
 
-def _pred_instances_to_mask(instances: Instances, *, score_thresh, mask_thresh):
+def _pred_instances_to_mask(instances: Instances, *, score_thresh):
     combined = np.zeros(instances.image_size, dtype=bool)
 
     try:
-        for score, mask in zip(instances.scores, instances.pred_masks):
+        for score, mask_bool in zip(instances.scores, instances.pred_masks):
             if score < score_thresh: continue
-            mask_bool = mask.detach().cpu().numpy() > mask_thresh
-            combined |= mask_bool
+            combined |= mask_bool.detach().cpu().numpy()
     except AttributeError:
         pass
 