50af7c967bebfbcdd612d719184ecb8a27337dc8
Chris Boyle <cbosoft@protonmail.com>
2022-04-08 09:10:22 +0100

    Set static number of columns for progress bar
    Makes it easier when working in tmux

--- dtron_cb/hooks/display_progress.py
+++ dtron_cb/hooks/display_progress.py
@@ -11,7 +11,7 @@ class DisplayProgressHook(HookBase):
         self.n_train_batches = self.n_valid_batches = 0
 
     def before_train(self):
-        self.bar = trange(self.trainer.n_epochs, unit='epochs')
+        self.bar = trange(self.trainer.n_epochs, unit='epochs', ncols=20)
 
     def after_train_batch(self):
         self.total_train_loss += self.trainer.this_batch_valid_loss
